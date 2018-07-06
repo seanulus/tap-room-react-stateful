6a7bc88a7ba1f2e56e2570e5d4d02c2ab5e3a842
Sean McDermott <seanjmac@hotmail.com>
2018-07-06 08:35:37 -0700

    Begin adding state to App component for purchase functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,22 +6,37 @@ import Header from './Header';
 import Footer from './Footer';
 import { Switch, Route } from 'react-router-dom';
 
-function App(){
-  return (
-    <div>
-      <style jsx>{`
-          background-color: black;
-          height: 1200px;
-      `}</style>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path= '/beers' component={BeersList} />
-        <Route exact path='/menu' component={MenuList} />
-      </Switch>
-      <Footer/>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      remaining: 120
+    }
+    this.handleBeersRemaining = this.handleBeersRemaining.bind(this);
+  }
+
+  handleBeersRemaining() {
+    
+  }
+
+  render() {
+
+    return (
+      <div>
+        <style jsx>{`
+            background-color: black;
+            height: 1200px;
+        `}</style>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path= '/beers' component={BeersList} />
+          <Route exact path='/menu' component={MenuList} />
+        </Switch>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
